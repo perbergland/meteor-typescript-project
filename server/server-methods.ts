1748c4b05422291553673a6a66d0ed79d9dd7c46
Per Bergland <per@refapp.se>
2020-07-22 09:41:24 +0200

    Readme and stuff

--- server/server-methods.ts
+++ server/server-methods.ts
@@ -1,6 +1,10 @@
 export const returnFour = () => 4;
 
 export const addFour = (n: number): number => {
+  const s = "Change the contents of this string to trigger rebuild";
+  if (!s) {
+    throw new Error("Whatever");
+  }
   const result = n + 4;
   return result;
 };
