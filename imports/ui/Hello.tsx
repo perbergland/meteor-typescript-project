c2647f60d4ac18572f88f2bb1b000b492a979836
Per Bergland <per@refapp.se>
2020-07-22 10:10:20 +0200

    adding import module

--- imports/ui/Hello.tsx
+++ imports/ui/Hello.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { getHello } from "../utils/import-methods";
 
 export const Hello = () => {
   const [counter, setCounter] = useState(0);
@@ -9,6 +10,7 @@ export const Hello = () => {
 
   return (
     <div>
+      <div>{getHello()}</div>
       <button onClick={increment}>Click Me</button>
       <p>You've pressed the button {counter} times.</p>
     </div>
--- server/server-methods.ts
+++ server/server-methods.ts
@@ -1,8 +1,11 @@
+import { getHello } from "/imports/utils/import-methods";
+
 export const returnFour = () => 4;
 
 export const addFour = (n: number): number => {
   const s = "Change the contents of this string to trigger rebuild";
-  if (!s) {
+  const t = getHello();
+  if (!s || !t) {
     throw new Error("Whatever");
   }
   const result = n + 4;
